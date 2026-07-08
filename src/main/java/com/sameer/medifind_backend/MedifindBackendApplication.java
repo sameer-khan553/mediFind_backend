8d9668367107c9a2972bf20d7f4eb1486081b64b
Sameer Khan <155846059+sameer-khan553@users.noreply.github.com>
2026-07-08 16:47:59 +0530

    project setup

---
+++ src/main/java/com/sameer/medifind_backend/MedifindBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sameer.medifind_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedifindBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedifindBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sameer/medifind_backend/MedifindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sameer.medifind_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedifindBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
